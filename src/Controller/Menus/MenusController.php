2271ca1cc233792956161b71d7531329954e2fcc
gdadzie <grace.dadzie2016@gmail.com>
2026-08-26 15:46:26 +0200

    deploy heroku

--- src/Controller/Menus/MenusController.php
+++ src/Controller/Menus/MenusController.php
@@ -29,21 +29,7 @@ class MenusController
         }
 
         try {
-            echo '<pre>';
-            echo '<pre>';
 
-            echo "PHP version : " . PHP_VERSION . "\n";
-            echo "upload_max_filesize : " . ini_get('upload_max_filesize') . "\n";
-            echo "post_max_size : " . ini_get('post_max_size') . "\n";
-            echo "file_uploads : " . ini_get('file_uploads') . "\n";
-
-            print_r($_FILES);
-
-            echo '</pre>';
-            exit;
-            print_r($_FILES);
-            echo '</pre>';
-            exit;
 
             $this->menuService->createMenu($_POST, $_FILES);
 
